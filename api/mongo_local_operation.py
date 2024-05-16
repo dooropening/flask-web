69b47dddc67064b12f7f85ed42dac16cf4dae970
dooropening <505258588@qq.com>
2024-05-16 11:03:16 +0800

    20240516 run normal on local machine.

--- api/mongo_local_operation.py
+++ api/mongo_local_operation.py
@@ -4,6 +4,5 @@ from pymongo import MongoClient
 
 mongodb_uri = os.environ.get('MONGODB_URI')
 client = MongoClient(mongodb_uri)
-db = client.quotesdb
-collection = db.quotes
-
+db = client.quotes_db
+collection = db.quotes_collection
